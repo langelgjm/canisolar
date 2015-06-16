71d5c3518a861c62dc19c5eafb0b94992ffab201
Gabriel J. Michael <gabriel.j.michael@gmail.com>
2015-06-15 21:28:34 -0400

    cleanup of OpenPV

--- bin/openpv.py
+++ bin/openpv.py
@@ -3,31 +3,36 @@
 Created on Fri Jun  5 09:23:38 2015
 
 @author: gjm
+
+This file contains the OpenPV class definiton.
+The OpenPV are available from https://openpv.nrel.gov/
 """
 
-import os
-os.chdir("/Users/gjm/insight/canisolar/bin/")
+#import os
+#os.chdir("/Users/gjm/insight/canisolar/bin/")
 import requests
 import us
 import pymysql.cursors
-import csv
-from pymysql import DataError
+#import csv
+#from pymysql import DataError
 import pandas as pd
 
-mysql_url = "localhost"
-mysql_db = "openpv"
-openpv_data_path = "/Users/gjm/insight/canisolar/data/openpv/"
+#db_url = "localhost"
+#db_name = "openpv"
+#openpv_data_path = "/Users/gjm/insight/canisolar/data/openpv/"
 
 
 class OpenPV(object):
     def __init__(self, db_url, db_name):
+        # This API key is available on the OpenPV site
         self.api_key = open("openpv_api_key.txt", "r").readline().rstrip()
         self.url = "http://developer.nrel.gov/api/solar/open_pv"
         self.csv_url = "http://developer.nrel.gov/api/solar/open_pv/installs/index"
+        # Create a list of two-letter state abbreviations (including DC)
         self.states = [state.abbr for state in us.states.STATES]
         self.db_url = db_url
         self.db_name = db_name
-        # charset utf8mb4 is the only proper way to handle true UTF-8 in mySQL.
+        # charset utf8mb4 is the only proper way to handle true UTF-8 in mySQL
         # First open connection to server without specifying db
         self.connection = pymysql.connect(host=self.db_url,
             user='root',
@@ -53,7 +58,7 @@ class OpenPV(object):
         with self.connection.cursor() as cursor:
             cursor.execute(sql)
         self.connection.commit()        
-    def create_tables(self):       
+    def create_tables(self):
         item_data = '''CREATE TABLE IF NOT EXISTS installs (
             id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
             zipcode CHAR(5), 
@@ -68,14 +73,14 @@ class OpenPV(object):
         self.connection.commit()        
     def get_api_state_data(self, state):
         '''
-        Return a link to a CSV file
+        Return a link to a CSV file for a given state.
         '''
         param_dict = {"export": "true", "api_key": self.api_key, "state": state, "pagenum": 1, "nppage": 25}
         r = requests.get(self.csv_url, params=param_dict)
         return r
     def get_api_state_csv(self, state):
         '''
-        Save a CSV file export for a given state. Will overwrite without warning.
+        Save a CSV file export for a given state. Will overwrite without warning!
         '''
         r = self.get_csv_data(state)
         filename = "openpv_" + state + ".csv"
@@ -84,11 +89,14 @@ class OpenPV(object):
         f.close()
     def get_api_all_csv(self):
         '''
-        Download CSV files for all states. Will overwrite without warning.
+        Save CSV files for all states. Will overwrite without warning!
         '''
         for state in self.states:
             self.get_api_state_csv(state)
     def insert_item_data(self, zipcode, state, size, cost, date_installed):
+        '''
+        Insert an install into the OpenPV MySQL database.        
+        '''
         # if a variable is missing, it's an empty string; set that to None instead so that it becomes NULL in the db
         zipcode = zipcode if zipcode != '' else None
         state = state if state != '' else None
@@ -99,53 +107,46 @@ class OpenPV(object):
         # note also that the % for date formatting needs to be escaped
         sql = '''INSERT INTO installs (zipcode, state, size, cost, date_installed) 
             VALUES (%s, %s, %s, %s, STR_TO_DATE(%s, '%%m/%%d/%%Y'))'''
-        #print(sql)
         with self.connection.cursor() as cursor:
             cursor.execute(sql, (zipcode, state, size, cost, date_installed))
         self.connection.commit()
-    def get_installs(self, state):
-        sql = '''SELECT * FROM installs WHERE state = %s'''
-        with self.connection.cursor() as cursor:
-            cursor.execute(sql, (state))
+    def get_installs(self, state=None):
+        '''
+        Return a pandas DataFrame of installs, optionally for a given state.
+        '''
+        if state:
+            sql = '''SELECT * FROM installs WHERE state = %s'''
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (state))
+        else:
+            sql = '''SELECT * FROM installs'''
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql)            
         results = cursor.fetchall()
         installs = pd.DataFrame(list(results), columns=['id', 'zipcode', 'state', 'size', 'cost', 'date_installed'])
         return installs
-    def get_similar_installs(self, state, size):
-        '''
-        Return 'similar' installs based on state and size. Similar defined to be +/- 10%
-        '''
-        state = str(state)
-        min_size = size * 0.90
-        max_size = size * 1.10
-        sql = '''SELECT * FROM installs WHERE state = %s AND size >= %s AND size <= %s'''
-        with self.connection.cursor() as cursor:
-            cursor.execute(sql, (state, str(min_size), str(max_size)))
-            results = cursor.fetchall()
-        # Need to  handles when there are no results
-        installs = pd.DataFrame(list(results), columns=['id', 'zipcode', 'state', 'size', 'cost', 'date_installed'])
-        return installs
-    def get_similar_installs_cost(self, state, size):
-        pass
     def close(self):
         self.connection.close()
 
 def main():
-    openpv = OpenPV(mysql_url, mysql_db)
-    
-    for filename in os.listdir(openpv_data_path):
-        if filename.endswith(".csv"):
-            with open(os.path.join(openpv_data_path, filename)) as csvfile:
-                reader = csv.reader(csvfile)
-                # skip the first line, which is a header
-                reader.__next__()
-                for row in reader:
-                    try:
-                        # Don't include the last two columns which are blank
-                        (zipcode, state, size, cost, date_installed) = row[:-2]
-                        openpv.insert_item_data(zipcode, state, size, cost, date_installed)
-                    except DataError:
-                        print(row)
-                        continue
+    pass
+# Code for populating MySQL database initially
+#    openpv = OpenPV(mysql_url, mysql_db)
+#    
+#    for filename in os.listdir(openpv_data_path):
+#        if filename.endswith(".csv"):
+#            with open(os.path.join(openpv_data_path, filename)) as csvfile:
+#                reader = csv.reader(csvfile)
+#                # skip the first line, which is a header
+#                reader.__next__()
+#                for row in reader:
+#                    try:
+#                        # Don't include the last two columns which are blank
+#                        (zipcode, state, size, cost, date_installed) = row[:-2]
+#                        openpv.insert_item_data(zipcode, state, size, cost, date_installed)
+#                    except DataError:
+#                        print(row)
+#                        continue
 
 if __name__ == "__main__":
     main()
